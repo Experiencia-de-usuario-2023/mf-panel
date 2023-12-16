8acdc6356290a55332ed192a191c1696894cbe13
Danaxar <daniel.catalan@usach.cl>
2023-12-16 19:15:05 -0300

    feat: Se prepara microfront

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const { dependencies } = require("./package.json");
+const webpack = require("webpack");
+const { DefinePlugin } = webpack;
+
+module.exports = {
+  entry: "./src/entry",
+  mode: "development",
+  devServer: {
+    port: 3020, // Modificar
+    historyApiFallback: true, // Esto hace que redirija siempre al index.html
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: "mf_panel", // Modificar
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Creacion": "./src/pages/Creacion.js",
+        "./EnReunion": "./src/pages/EnReunion.js",
+        "./PostReunion": "./src/pages/PostReunion.js",
+        "./PreReunion": "./src/pages/PreReunion.js",
+      },
+      shared: {
+        ...dependencies,
+        react: {
+          singleton: true,
+          requiredVersion: dependencies["react"],
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: dependencies["react-dom"],
+        },
+      },
+    }),
+    new webpack.DefinePlugin({
+      "process.env.backend_url": JSON.stringify("http://172.111.10.181/api/"),
+    }),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+  target: "web",
+};
+
+// Solo modificar las lineas que tienen comentarios
